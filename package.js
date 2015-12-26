9f7c28438c997bf9eba83fea84c0da7980c365f6
Veera Marni <veeramarni@Veeras-MacBook-Pro.local>
2015-12-26 12:51:55 -0500

    upgraded codova-plugin-device to 1.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'okland:camera-ui',
-  version: '0.0.2',
+  name: 'veeramarni:camera-ui',
+  version: '0.0.3',
   summary: 'Take photos with UI one function call on desktop and mobile. Choose between camera to photoLibrary.',
   git: 'https://github.com/okland/camera-ui',
   documentation: 'README.md'
@@ -8,13 +8,13 @@ Package.describe({
 
 Cordova.depends({
   "nl.x-services.plugins.actionsheet": "https://github.com/EddyVerbruggen/cordova-plugin-actionsheet/tarball/df5e89e54d5142c964c1139db6fe813a01125825",
-  "org.apache.cordova.device": "0.3.0"
+  "org.apache.cordova.device": "1.1.0"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2');
   api.export('MeteorCameraUI');
-  api.use('mdg:camera@1.1.4');
+  api.use('mdg:camera@1.2.1');
   api.addFiles('camera-ui.js');
   api.addFiles('camera-ui-client.js', ['web.browser', 'web.cordova']);
   api.addFiles('camera-ui-browser.js', ['web.browser']);
